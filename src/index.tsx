7778e0b6212badf5cba3be340a1fe6467a3a9ea4
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-03-23 22:31:59 +0700

    feat: implement the sidebar component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Routes from "./router/router.tsx";
+
+import {ChakraProvider} from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <div>Hello World</div>
-  </React.StrictMode>,
+    <ChakraProvider>
+        <React.StrictMode>
+            <Routes/>
+        </React.StrictMode>
+    </ChakraProvider>
 )
